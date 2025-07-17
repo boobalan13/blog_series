2f68fd87deef290068dd45c552e9a380e7b2c847
boobalan13 <boobalan.rs2023cse@sece.ac.in>
2025-07-17 23:24:52 +0530

    up

--- Component/Display.js
+++ Component/Display.js
@@ -1,27 +1,32 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BlogBox from "./BlogBox";
-import data from "@/public/dataSet.json";
 import { useRouter } from "next/navigation";
+import defaultDataset from "./defaultDataset"; 
 
 const Display = () => {
   const router = useRouter();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("episodes")) || defaultDataset;
+    setData(storedData);
+  }, []);
+
   return (
     <div className="blog_area">
-      {data.map((item, index) => {
-        return (
-          <BlogBox
-            key={index}
-            episode_id={item.episode_id}
-            episode_name={item.episode_name}
-            episode_image={item.episode_image}
-            description={item.description}
-            duration={item.duration}
-            release_date={item.release_date}
-            router={router}
-          />
-        );
-      })}
+      {data.map((item, index) => (
+        <BlogBox
+          key={index}
+          episode_id={item.episode_id}
+          episode_name={item.episode_name}
+          episode_image={item.episode_image}
+          description={item.description}
+          duration={item.duration}
+          release_date={item.release_date}
+          router={router}
+        />
+      ))}
     </div>
   );
 };
